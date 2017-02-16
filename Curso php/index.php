e340f366f1b174ed91577062f000d7aaae7cdfa3
mxcr <maxlorarc@gmail.com>
2017-02-15 22:04:21 -0600

    clase 1

---
+++ Curso php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Inicio Curso php</title>
+</head>
+<body>
+<?php
+print"<h1>Bienvenidos al curso php</h1> <br>";
+print "Hola alumnos<br>";
+print "Hasta el proxim video"
+?>
+
+</body>
+</html>
