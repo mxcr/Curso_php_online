35e6f8bb0a8e5ba7c13897e8d1136dc1cd5d9fa3
mxcr <maxlorarc@gmail.com>
2017-02-17 21:21:26 -0600

    static variables
    variables estaticas

--- Curso php/index.php
+++ Curso php/index.php
@@ -2,19 +2,16 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Inicio Curso php(Ambito de las variables))</title>
+	<title>Inicio Curso php(variables estaticas))</title>
 </head>
 <body>
 <?php
-$nombre="Juan";
-
-function dameNombre(){
-	global $nombre;
-	$nombre="El nombre es: ". $nombre;
-
+function incrementaVariable(){
+	static $contador=0;
+	$contador++;
+	echo $contador."<br>";
 }
-dameNombre();
-echo $nombre;
+ incrementaVariable(); incrementaVariable(); incrementaVariable(); incrementaVariable();
 ?>
 
 </body>
