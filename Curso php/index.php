ecb0a31ce6d3c79f146f4dabbb55d7d0994758fa
mxcr <maxlorarc@gmail.com>
2017-02-17 22:02:09 -0600

    Video 8 comparacion de string
    strcmp->compara sin tomar en cuenta mayusculas
    strcasecmp->compara tomndo en cuenta mayusculas

--- Curso php/index.php
+++ Curso php/index.php
@@ -2,16 +2,32 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Inicio Curso php(variables estaticas))</title>
+	<title>Inicio Curso php(STRINGS))</title>
+	<style>
+		.resaltar{
+			color: #F00;4
+			font-weight: bold;
+		}
+	</style>
 </head>
 <body>
 <?php
-function incrementaVariable(){
-	static $contador=0;
-	$contador++;
-	echo $contador."<br>";
+
+echo "<p class='resaltar'>Esto es un ejemplo de frase</p>";
+
+echo "<p class=\"resaltar\">Esto es un ejemplo de frase</p>";
+
+$variable1="casa";
+$variable2="CASA";
+
+$resultado=strcasecmp($variable1, $variable2);
+
+if($resultado){
+	echo "Coinciden";
+}else{
+	echo "No coinciden";
 }
- incrementaVariable(); incrementaVariable(); incrementaVariable(); incrementaVariable();
+
 ?>
 
 </body>
